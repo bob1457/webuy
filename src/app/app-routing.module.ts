67dc932dac01cd7301e05cf9c7dba643d828ee2f
bob1457 <bob.yuanu@yahoo.com>
2021-10-09 17:31:01 -0700

    Create a dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'register',
+    loadChildren: () => import('./auth/register/register.module').then( m => m.RegisterPageModule)
   },
+
 ];
 
 @NgModule({
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  subjects;
+
+  constructor(private router: Router) {}
+  ngOnInit(): void {
+    this.subjects = [
+      {
+        name: 'All',
+        image: 'http://placehold.it/80x80',
+      },
+      {
+        image: 'http://placehold.it/80x80',
+        name: 'English'
+      },
+      {
+        image: 'http://placehold.it/80x80',
+        name: 'Chemistry'
+      },
+      {
+        image: 'http://placehold.it/80x80',
+        name: 'English'
+      },
+      {
+        image: 'http://placehold.it/80x80',
+        name: 'Social'
+      },
+      {
+        image: 'http://placehold.it/80x80',
+        name: 'Physics'
+      },
+      {
+        image: 'http://placehold.it/80x80',
+        name: 'Math'
+      },
+      {
+        image: 'http://placehold.it/80x80',
+        name: 'Math'
+      }
+    ];
+  }
+
+  goToSubject() {
+
+  }
 
 }
