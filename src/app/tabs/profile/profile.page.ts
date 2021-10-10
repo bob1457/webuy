f105a3a6a3fe27ae6182dad517199965777673c3
bob1457 <bob.yuanu@yahoo.com>
2021-10-10 16:05:41 -0700

    Added more UI element on segments

--- src/app/tabs/profile/profile.page.ts
+++ src/app/tabs/profile/profile.page.ts
@@ -11,13 +11,16 @@ export class ProfilePage implements OnInit {
   stories = [];
   buttonValue = 'grid';
   buttonItems = [];
+  posts = [];
 
   constructor() { }
 
   ngOnInit() {
+
     this.slideOptions = {
       slidesPerView: 5.5,
     };
+
     this.stories = [
       {name: 'New'},
       {name: 'Android', src: '../assets/images/circles/android.png'},
@@ -29,10 +32,26 @@ export class ProfilePage implements OnInit {
       {name: 'Php', src: '/assets/images/circles/php.png'} //,
       // {name: 'Android', src: 'assets/images/android.png'},
     ];
+
     this.buttonItems = [
       {value: 'grid', icon: 'grid'},
       {value: 'reels', icon: 'document'},
       {value: 'photos', icon: 'images'}
+    ];
+
+    this.posts = [
+      { id: 1, url: '../assets/images/posts/1.jpg'},
+      { id: 2, url: '../assets/images/posts/2.jpg'},
+      { id: 3, url: '../assets/images/posts/3.png'},
+      { id: 4, url: '../assets/images/posts/4.png'},
+      { id: 9, url: '../assets/images/posts/5.jpg'},
+      { id: 6, url: '../assets/images/posts/6.png'},
+      { id: 5, url: '../assets/images/posts/7.png'},
+      { id: 8, url: '../assets/images/posts/8.jpg'},
+      { id: 7, url: '../assets/images/posts/9.png'},
+      { id: 10, url: '../assets/images/posts/10.png'},
+      { id: 11, url: '../assets/images/posts/11.png'},
+      { id: 12, url: '../assets/images/posts/12.png'},
     ];
   }
 
