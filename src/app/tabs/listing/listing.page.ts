dcd957865bf8ed62c23cce3d3ad86b4bb36914fa
bob1457 <bob.yuanu@yahoo.com>
2021-10-10 23:13:10 -0700

    Finished adding listing page

--- src/app/tabs/listing/listing.page.ts
+++ src/app/tabs/listing/listing.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-listing',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListingPage implements OnInit {
 
-  constructor() { }
+  @ViewChild(IonSlides) ionSlides: IonSlides;
+  slideOpts = {};
+  slides = [];
+  activities = [];
+  activityTab = 0;
+
+  constructor() {
+  }
 
   ngOnInit() {
+
+    this.slideOpts = {
+      slidesPerView: 4.5,
+      slideshow: true,
+    };
+
+    this.slides = [
+      {id: 1, name: 'All Activity'},
+      {id: 2, name: 'Likes', notifications: 32},
+      {id: 3, name: 'Comments', notifications: 15},
+      {id: 4, name: 'Mentions'}
+    ];
+
+    this.activities = [
+      {id: 1, username: 'jasonlau', userProfile: '../assets/images/2.png', time: '1 min', activity: 'follow'},
+      // eslint-disable-next-line max-len
+      {id: 2, username: 'ericmiller', userProfile: '../assets/images/6.png', time: '5 mins', activity: 'comment', comment: 'Good job', post: '../assets/images/posts/12.png'},
+      {id: 3, username: 'divdn', userProfile: '../assets/images/3.png', time: '1d', activity: 'follow'},
+      {id: 4, username: 'nickchan', userProfile: '../assets/images/4.png', time: '1w', activity: 'follow'},
+      // eslint-disable-next-line max-len
+      {id: 5, username: 'iq.code', userProfile: '../assets/images/5.png', time: '3 mins', activity: 'comment', comment: 'Thank for your great work', post: '../assets/images/posts/7.png'},
+      {id: 6, username: 'jasonlau', userProfile: '../assets/images/1.png', time: '2w', activity: 'follow'}
+    ];
+  }
+
+  changeTab(index){
+    this.activityTab = index;
+    this.ionSlides.slideTo(index,);
   }
 
 }
