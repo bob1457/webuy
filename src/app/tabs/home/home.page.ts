9701a00ff8eaaed46bafaaecd2bfcfc091f513e8
bob1457 <bob.yuanu@yahoo.com>
2021-10-16 00:30:41 -0700

    Added home page on tabs sub app

--- src/app/tabs/home/home.page.ts
+++ src/app/tabs/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +7,124 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
+  @ViewChildren('instaVideo') videos: QueryList<any>;
+  feeds: any[] = [
+    {
+      id: 1,
+      logo: 'assets/images/logo.png',
+      username: 'Coding Technyks',
+      location: 'India',
+      src: 'assets/videos/1.mp4',
+      // eslint-disable-next-line max-len
+      description: 'Hello Friends, Welcome to @CodingTechnyks, I brought to you Instagram Clone App UI - Part I using​ #Ionic5 #Angular for #android & #ios. Instagram Profile & Activity Screens. Watch at https://youtu.be/zQIBTFaDbNI',
+      likes: 15
+    },
+    {
+      id: 2,
+      logo: 'assets/images/logo.png',
+      username: 'Coding Technyks',
+      location: 'India',
+      src: 'assets/images/posts/7.png',
+      description: 'Music App Auth Screens. Watch at https://www.youtube.com/watch?v=AH0Eaubds7w&t=89s&ab_channel=CodingTechnyks',
+      likes: 80,
+      image: true
+    },
+    {
+      id: 3,
+      logo: 'assets/images/logo.png',
+      username: 'Coding Technyks',
+      location: 'India',
+      src: 'assets/videos/2.mp4',
+      // eslint-disable-next-line max-len
+      description: "Ionic 5+ Bottom Sheet 2021 #instagramapp #CodingTechnyks #Technyks #Ionic UI #Angular #shorts ► Hello Friends, Welcome to @CodingTechnyks, I brought to you Ionic 5+ Bottom Sheet UI Design using​ #Ionic5 #Angular for #android & #ios. Full Episode at Ionic 5+ Bottom Sheet 2021 - Implementing Modal with Dynamic list | Instagram Clone App | Part III",
+      likes: 4
+    },
+    {
+      id: 4,
+      logo: 'assets/images/logo.png',
+      username: 'Coding Technyks',
+      location: 'India',
+      src: 'assets/videos/3.mp4',
+      // eslint-disable-next-line max-len
+      description: 'Its Nature Again! A Brand New "PLANT APP - Part II" published by #codingtechnyks using​ #Ionic5 - #Angular #Typescript #ionic for #android & #ios, can also be used for #pwa',
+      likes: 56
+    },
+    {
+      id: 5,
+      logo: 'assets/images/logo.png',
+      username: 'Coding Technyks',
+      location: 'India',
+      src: 'assets/images/posts/4.png',
+      // eslint-disable-next-line max-len
+      description: 'Food Delivery App Auth Screens. Watch at https://www.youtube.com/watch?v=Td46HaHVR0g&list=PLixvNT19uDK6Fk6glTj18ONQRpCAVnY0G&ab_channel=CodingTechnyks',
+      likes: 150,
+      image: true
+    },
+    {
+      id: 6,
+      logo: 'assets/images/logo.png',
+      username: 'Coding Technyks',
+      location: 'India',
+      src: 'assets/images/posts/10.png',
+      description: 'Plant App part II. Watch at https://www.youtube.com/watch?v=ZNRr8lg-kDU&t=1136s&ab_channel=CodingTechnyks',
+      likes: 200,
+      image: true
+    },
+    {
+      id: 7,
+      logo: 'assets/images/logo.png',
+      username: 'Coding Technyks',
+      location: 'India',
+      src: 'assets/images/posts/6.png',
+      description: 'E-Commerce App Auth Screens. Watch at https://www.youtube.com/watch?v=vdRYkwImbYQ&t=315s&ab_channel=CodingTechnyks',
+      likes: 100,
+      image: true
+    }
+  ];
+
+  nowPlaying = null;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  isElementInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  didScroll(event?) {
+    console.log(event);
+    if(this.nowPlaying && this.isElementInViewport(this.nowPlaying)) return;
+    else if(this.nowPlaying && !this.isElementInViewport(this.nowPlaying)) {
+      this.nowPlaying.pause();
+      this.nowPlaying = null;
+    }
+
+    this.videos.forEach(player => {
+      console.log('player', player);
+
+      if(this.nowPlaying) return;
+
+      const nativeElement = player.nativeElement;
+      const inView = this.isElementInViewport(nativeElement);
+
+      if(inView) {
+        this.nowPlaying = nativeElement;
+        this.nowPlaying.muted = true;
+        this.nowPlaying.play();
+      }
+    });
+  }
+
+  toogleWrap(feed) {
+    feed.wrap = !feed.wrap;
   }
 
 }
